43a1cee0a36029d9bebae519785f38cdc86bf3d8
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2009-03-25 09:31:26 +0000

    Import of TOKUHIROM/Test-Fork-0.01 from CPAN.
    gitpan-cpan-distribution: Test-Fork
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TOKUHIROM/Test-Fork-0.01.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Test/Fork.pm
+++ lib/Test/Fork.pm
@@ -1,191 +1,128 @@
 package Test::Fork;
-
 use strict;
 use warnings;
+our $VERSION = '0.01';
+use Test::Builder;
+use Test::Fork::Scalar;
+use Test::Fork::Array;
+use Test::Fork::Store;
+use IPC::ShareLite ':lock';
+use Storable ();
 
-our $VERSION = '0.02';
+our $TEST;
+sub import {
+    $TEST = Test::Builder->new();
 
-use base 'Test::Builder::Module';
-our @EXPORT = qw(fork_ok);
-
-my $CLASS = __PACKAGE__;
-
-
-sub note {
-    my $msg = shift;
-    my $fh = $CLASS->builder->output;
-
-    print $fh "# $msg\n";
+    my $store = Test::Fork::Store->new();
+    $store->lock_cb(sub {
+        $store->{share}->store(Storable::nfreeze(+{
+            array => [],
+            scalar => 0,
+        }));
+    }, LOCK_EX);
 }
 
+my $CLEANUPME;
+sub parent {
+    my $store = _setup();
+    $CLEANUPME = $store;
+}
 
-=head1 NAME
+sub child {
+    # And musuka said: 'ラピュタは滅びぬ！何度でもよみがえるさ！'
+    # (Quote from 'LAPUTA: Castle in he Sky')
+    $TEST->no_ending(1);
 
-Test::Fork - test code which forks
+    _setup();
+}
 
-=head1 SYNOPSIS
+sub _setup {
+    my $store = Test::Fork::Store->new();
+    tie $TEST->{Curr_Test}, 'Test::Fork::Scalar', 0, $store;
+    tie @{$TEST->{Test_Results}}, 'Test::Fork::Array', $store;
 
-    use Test::More tests => 4;
-    use Test::Fork;
-    
-    fork_ok(2, sub{
-        pass("Test in the child process");
-        pass("Another test in the child process");
-    });
-    
-    pass("Test in the parent");
-
-=head1 DESCRIPTION
-
-B<THIS IS ALPHA CODE!>  The implementation is unreliable and the interface
-is subject to change.
-
-Because each test has a number associated with it, testing code which forks
-is problematic.  Coordinating the test number amongst the parent and child
-processes is complicated.  Test::Fork provides a function to smooth over
-the complications.
-
-=head2 Functions
-
-Each function is exported by default.
-
-=head3 B<fork_ok>
-
-    my $child_pid = fork_ok( $num_tests, sub {
-        ...child test code...
-    });
-
-Runs the given child test code in a forked process.  Returns the pid of the
-forked child process, or false if the fork fails.
-
-$num_tests is the number of tests in your child test code.
-Consider it to be a sub-plan.
-
-fork_ok() itself is a test, if the fork fails it will fail.  fork_ok()
-test does not count towards your $num_tests.
-
-    # This is three tests.
-    fork_ok( 2, sub {
-        is $foo, $bar;
-        ok Something->method;
-    });
-
-The children are automatically reaped.
-
-=cut
-
-my %Reaped;
-my %Running_Children;
-my $Is_Child = 0;
-
-sub fork_ok ($&) {
-    my($num_tests, $child_sub) = @_;
-    
-    my $tb = $CLASS->builder;    
-    my $pid = fork;
-
-    # Failed fork
-    if( !defined $pid ) {
-        return $tb->ok(0, "fork() failed: $!");
-    }
-    # Parent
-    elsif( $pid ) {
-        # Avoid race condition where child has run and is reaped before
-        # parent even runs.
-        $Running_Children{$pid} = 1 unless $Reaped{$pid};
-
-        $tb->use_numbers(0);
-        $tb->current_test($tb->current_test + $num_tests);
-
-        $tb->ok(1, "fork() succeeded, child pid $pid");
-        return $pid;
-    }
-
-    # Child
-    $Is_Child = 1;
-
-    $tb->use_numbers(0);
-    $tb->no_ending(1);
-    
-    note("Running child pid $$");
-    $child_sub->();
-    exit;
+    no strict 'refs';
+    no warnings 'redefine';
+    for my $name (qw/ok skip todo_skip current_test/) {
+        my $cur = *{"Test::Builder::${name}"}{CODE};
+        *{"Test::Builder::${name}"} = sub {
+            my @args = @_;
+            $store->lock_cb(sub {
+                $cur->(@args);
+            }, LOCK_EX);
+        };
+    };
+    return $store;
 }
 
 END {
-    while( !$Is_Child and keys %Running_Children ) {
-        note("reaper($$) waiting on @{[keys %Running_Children]}");
-        _check_kids();
-        _reaper();
+    if ($CLEANUPME) {
+        $CLEANUPME->{share}->destroy(1);
     }
 }
 
-sub _check_kids {
-    for my $child (keys %Running_Children) {
-        delete $Running_Children{$child} if $Reaped{$child};
-        delete $Running_Children{$child} unless kill 0, $child;
-        note("Child $child already reaped");
+1;
+__END__
+
+=head1 NAME
+
+Test::Fork - fork test
+
+=head1 SYNOPSIS
+
+    use Test::More tests => 200;
+    use Test::Fork;
+
+    my $pid = fork();
+    if ($pid == 0) {
+        # child
+        Test::Fork->child;
+        ok 1, "child $_" for 1..100;
+    } elsif ($pid) {
+        # parent
+        Test::Fork->parent;
+        ok 1, "parent $_" for 1..100;
+        waitpid($pid, 0);
+    } else {
+        die $!;
     }
-}
 
-sub _reaper {
-    local $?;  # wait sets $?
+=head1 DESCRIPTION
 
-    my $child_pid = wait;
-    $Reaped{$child_pid}++;
-    delete $Running_Children{$child_pid};
+Test::Fork is utility module for Test::Builder.
+This module makes forking test!
 
-    note("child $child_pid reaped");
+This module merges test count with parent process & child process.
 
-    $CLASS->builder->use_numbers(1) unless keys %Running_Children;
+=head1 METHODS
 
-    return $child_pid == -1 ? 0 : 1;
-}
+=over 4
 
-$SIG{CHLD} = \&_reaper;
+=item parent
 
+call this class method, if you are parent
 
-=head1 CAVEATS
+=item child
 
-The failure of tests in a child process cannot be detected by the parent.
-Therefore, the normal end-of-test reporting done by Test::Builder will
-not notice failed child tests.
+call this class method, if you are child.
 
-Test::Fork turns off test numbering in order to avoid test counter
-coordination issues.  It turns it back on once the children are done
-running.
+you can call this method many times(maybe the number of your children).
 
-Test::Fork will wait for all your child processes to complete at the end of
-the parent process.
-
-=head1 SEE ALSO
-
-L<Test::MultiFork>
-
+=back
 
 =head1 AUTHOR
 
-Michael G Schwern E<lt>schwern@pobox.comE<gt>
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
 
+yappo
 
-=head1 BUGS and FEEDBACK
+=head1 SEE ALSO
 
-Please send all bugs and feature requests to 
-I<bug-Test-Fork> at I<rt.cpan.org> or use the web interface via
-L<http://rt.cpan.org>.
+L<Test::TCP>
 
-If you use it, please send feedback.  I like getting feedback.
+=head1 LICENSE
 
-
-=head1 COPYRIGHT and LICENSE
-
-Copyright 2007-2008 by Michael G Schwern E<lt>schwern@pobox.comE<gt>.
-
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-See F<http://www.perl.com/perl/misc/Artistic.html>
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
-
-42;
